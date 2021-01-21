2f649c1306264d3b44cafbe495c15951572b1ef6
Sandy Long <sandylx33@gmail.com>
2021-01-20 22:17:09 -0500

    refactored class components to functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,96 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 import youtube from '../APIs/youtube';
 
-export default class App extends React.Component {
 
-  state = {videos: [], selectedVideo: null}
 
-  //will do a default term search as soon as App loads so that the page doesn't have to be empty at first
-  componentDidMount() {
-    this.onTermSubmit('twice');
-  }
+export default function App() {
 
-  onTermSubmit = async (term) => {
+  const [videos, setVideos] = useState([])
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    
+    onTermSubmit('Twice');
+  
+  }, []);
+
+  const onTermSubmit = async (term) => {
     //youtube is a pre-configured instance of axios
     const response = await youtube.get('/search', {
       params: {
-        q: term //the querying term is this.state.term in SearchBar
+        q: term 
       }
-    })
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
     });
-  };
-
-  onVideoSelect = (video) => {
-    this.setState({selectedVideo: video})
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   }
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
+  return (
+    <div className="ui container">
+        <SearchBar onFormSubmit={onTermSubmit}/>
         <div className="ui grid">
           <div className="ui row">
             <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo}/>
+              <VideoDetail video={selectedVideo}/>
             </div>
             <div className="five wide column">
-              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+              <VideoList 
+                videos={videos} 
+                onVideoSelect={(video) => setSelectedVideo(video)}
+              />
             </div>
           </div>
         </div>    
       </div>
-    )
-  }
+  )
 }
+
+// Class components 
+// export default class App extends React.Component {
+
+//   state = {videos: [], selectedVideo: null}
+
+//   //will do a default term search as soon as App loads so that the page doesn't have to be empty at first
+//   componentDidMount() {
+//     this.onTermSubmit('twice');
+//   }
+
+//   onTermSubmit = async (term) => {
+//     //youtube is a pre-configured instance of axios
+//     const response = await youtube.get('/search', {
+//       params: {
+//         q: term //the querying term is this.state.term in SearchBar
+//       }
+//     })
+//     this.setState({
+//       videos: response.data.items,
+//       selectedVideo: response.data.items[0]
+//     });
+//   };
+
+//   onVideoSelect = (video) => {
+//     this.setState({selectedVideo: video})
+//   }
+
+//   render() {
+//     return (
+//       <div className="ui container">
+//         <SearchBar onFormSubmit={this.onTermSubmit}/>
+//         <div className="ui grid">
+//           <div className="ui row">
+//             <div className="eleven wide column">
+//               <VideoDetail video={this.state.selectedVideo}/>
+//             </div>
+//             <div className="five wide column">
+//               <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+//             </div>
+//           </div>
+//         </div>    
+//       </div>
+//     )
+//   }
+// }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-export default class SearchBar extends React.Component {
+export default function SearchBar(props) {
 
-  state = {term: ''};
+  const [term, setTerm] = useState('');
 
-  onFormSubmit = evt => {
-    evt.preventDefault();
-    
-    this.props.onFormSubmit(this.state.term);
+  const onFormSubmit = evt => {
+    evt.preventDefault(); 
+    props.onFormSubmit(term);
   }
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
+  return (
+    <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input 
             type="text"
-            value={this.state.term}
-            onChange={(evt) => this.setState({term: evt.target.value})}
+            value={term}
+            onChange={(evt) => setTerm(evt.target.value)}
             />
           </div>
         </form>
       </div>
-    )
-  }
+  )
 }
+
+//Class Component
+// export default class SearchBar extends React.Component {
+
+//   state = {term: ''};
+
+//   onFormSubmit = evt => {
+//     evt.preventDefault();
+    
+//     this.props.onFormSubmit(this.state.term);
+//   }
+
+//   render() {
+//     return (
+//       <div className="search-bar ui segment">
+//         <form className="ui form" onSubmit={this.onFormSubmit}>
+//           <div className="field">
+//             <label>Video Search</label>
+//             <input 
+//             type="text"
+//             value={this.state.term}
+//             onChange={(evt) => this.setState({term: evt.target.value})}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
